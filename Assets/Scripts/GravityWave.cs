1be05e9b04f4ccb64277d1e8f4f01ccb41dc1859
ConnorMackay <1002585@student.sae.edu.au>
2016-03-10 17:03:31 +1000

    Gravity wave basic

---
+++ Assets/Scripts/GravityWave.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityWave : MonoBehaviour {
+	public Rigidbody2D playerRB; 
+
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D>().AddForce (transform.right * -200);
+	}
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		//if (other.gameObject = 
+		playerRB.AddForce(transform.right * -500); 
+
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
