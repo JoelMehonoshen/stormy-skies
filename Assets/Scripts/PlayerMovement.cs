681dbae70c6e78f244e70ca3559d1a1a79ebd4b1
Joel Mehonoshen <joel.mehonoshen@hotmail.com>
2016-03-10 14:28:21 +1000

    Player movement and camera follow.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public Rigidbody2D playerRB;
+	public float speed;
+	public float turnSpeed;
+	// Use this for initialization
+	void Start () {
+		playerRB = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			playerRB.AddForce( transform.up * Time.deltaTime * speed);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			playerRB.AddForce(transform.up * Time.deltaTime * -speed);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (Vector3.back * Time.deltaTime*turnSpeed);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (Vector3.forward * Time.deltaTime*turnSpeed);
+		}
+	}
+}
