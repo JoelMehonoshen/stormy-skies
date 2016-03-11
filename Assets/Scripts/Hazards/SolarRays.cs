8f6104b7c6bc504a40e837fa2a2eb01f622b0d79
ConnorMackay <1002585@student.sae.edu.au>
2016-03-11 14:06:44 +1000

    Created seperate folder for hazards. Solar Ray prototype exissts

---
+++ Assets/Scripts/Hazards/SolarRays.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SolarRays : MonoBehaviour {
+	public GameObject playerController;
+
+
+	// Use this for initialization
+	void Start () {
+		//playerController = GetComponent<PlayerController> ();
+	}
+
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+
+			playerController.GetComponent<PlayerController> ().hitWall ();
+
+
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
