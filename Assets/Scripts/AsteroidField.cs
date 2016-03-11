cb9a109a0bf987bf3624f5c94a8b085747a6840c
ConnorMackay <1002585@student.sae.edu.au>
2016-03-11 13:21:03 +1000

    My intention for the Asteroid Field is that when the player enters it's trigger zone, it will look at the player and 'shoot' asteoids in their general direction. The Look At part is in.

---
+++ Assets/Scripts/AsteroidField.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidField : MonoBehaviour {
+	private PlayerController player; 
+	public Transform theplayer;
+	public bool looking; 
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindObjectOfType<PlayerController> (); 
+	}
+
+	void OnTriggerEnter2D (Collider2D other) 
+	{
+		if (player) 
+		{
+			transform.LookAt (theplayer);
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+	}
+}
